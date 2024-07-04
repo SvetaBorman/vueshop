591165f787d08a478eeaae23fc7a030e575fb5c9
SvetaBormatova <svpanaeva@yandex.ru>
2024-07-04 17:08:22 +0500

    create data items

---
+++ src/data/item.js
@@ -0,0 +1,18 @@
+const items = [
+	{
+		id:1,
+		name: "Number 1",
+		description: "Дополнительная более подробная информация о товаре",
+		price: 100,
+		category: "category1"
+	},
+	{
+		id:2,
+		name: "Number 2",
+		description: "Дополнительная более подробная информация о товаре",
+		price: 200,
+		category: "category2"
+	}
+]
+
+export default items
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-	 <TestBasket/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import TestBasket from "@/components/TestBasket.vue"
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld, TestBasket
+   
   }
 }
 </script>
